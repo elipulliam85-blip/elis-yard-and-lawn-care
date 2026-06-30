eb9cc4c0a15f77247d48f4a7f0074d41d5f5b1df
elipulliam85-blip <elipulliam85@gmail.com>
2026-06-29 21:35:50 -0800

    my website

---
+++ script.js
@@ -0,0 +1,13 @@
+const faders = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+faders.forEach(section => {
+    observer.observe(section);
+});
